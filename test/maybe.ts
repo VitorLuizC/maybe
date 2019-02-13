8a800200b839f588881a5fa7703e9a59e96564af
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2019-02-13 20:03:50 -0200

    :white_check_mark: create .get and Maybe.isMaybe tests + improve .map tests

--- test/maybe.ts
+++ test/maybe.ts
@@ -1,6 +1,8 @@
 import test from 'ava';
 import Maybe from '../';
 
+// ..:: Maybe factory tests ::..
+
 test('Maybe typed value can be `null`, `undefined` or `T`', (context) => {
   const valueA = Maybe<number>(undefined);
 
@@ -15,13 +17,7 @@ test('Maybe typed value can be `null`, `undefined` or `T`', (context) => {
   context.is(valueC.get(0), 10);
 });
 
-test('Maybe enforce type safely flows', (context) => {
-  const name = Maybe('Naruto Uzumaki');
-
-  const names = name.map((name) => name.replace(/\s+/, ' ').split(' '));
-
-  context.deepEqual(names.get([]), ['Naruto', 'Uzumaki']);
-});
+// ..:: Maybe.isNone tests ::..
 
 test('Maybe.isNone check if value is none', (context) => {
   context.true(Maybe.isNone(null));
@@ -34,6 +30,29 @@ test('Maybe.isNone doesn\'t count falsy values as none', (context) => {
   context.false(Maybe.isNone(NaN));
   context.false(Maybe.isNone(false));
 });
+
+// ..:: Maybe.isMaybe tests ::..
+
+test('Maybe.isMaybe check if value is a Maybe', (context) => {
+  context.true(Maybe.isMaybe(Maybe()));
+  context.false(Maybe.isMaybe({}));
+  context.false(Maybe.isMaybe(NaN));
+  context.false(Maybe.isMaybe(undefined));
+});
+
+// ..:: maybe.get tests ::..
+
+test('Maybe.get returns value or placeholder if it\'s none', (context) => {
+  const size = Maybe<number>();
+
+  context.is(size.get(0), 0);
+
+  const font = Maybe<string>('Operator Mono');
+
+  context.is(font.get(''), 'Operator Mono');
+});
+
+// ..:: maybe.map tests ::..
 
 test('Maybe.map call fn if value is some', (context) => {
   const size = Maybe<number>(undefined);
@@ -53,19 +72,30 @@ test('Maybe.map call fn if value is some', (context) => {
   context.true(fnWasCalledForFont);
 });
 
-test('Maybe.map returns a Maybe typed value', (context) => {
-  const value = Maybe<number>(150);
+test('Maybe.map returns empty Maybe if value is None', (context) => {
+  const value = Maybe<number>().map(() => 1000);
 
-  const hash = value.map((value) => {
-    return Math.ceil(value).toString(16).substr(0, 8).padStart(8, '0');
-  });
+  context.true(Maybe.isMaybe(value));
 
-  const key = 'id-' + hash.get('00000000');
+  let fnWasCalled = false;
 
-  context.is(key, 'id-00000096');
+  value.then(() => fnWasCalled = true);
+
+  context.false(fnWasCalled);
 });
 
-// ..:: Maybe.then tests ::..
+test('Maybe.map resolve Maybe<Maybe<any>> to Maybe<any>', (context) => {
+  const value = Maybe<number>(100).map((value) => Maybe(value));
+
+  context.true(Maybe.isMaybe(value));
+
+  value.then((value) => {
+    context.is(value, 100)
+    context.false(Maybe.isMaybe(value))
+  });
+});
+
+// ..:: maybe.then tests ::..
 
 test('Maybe.then runs fn if value is some', (context) => {
   const size = Maybe<number>(undefined);
