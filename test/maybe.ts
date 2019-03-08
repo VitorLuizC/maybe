3effc73f2c5a9fbe66dfe9b36312c1cf0e0859bc
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2019-03-08 13:12:59 -0300

    :white_check_mark: fix Maybe name on tests and apply None and Some

--- test/maybe.ts
+++ test/maybe.ts
@@ -1,10 +1,10 @@
 import test from 'ava';
-import Maybe, { isMaybe, None, Some } from '../';
+import createMaybe, { isMaybe, None, Some } from '../';
 
 // ..:: isMaybe ::..
 
 test('isMaybe check if value is a Maybe', (context) => {
-  context.true(isMaybe(Maybe()));
+  context.true(isMaybe(None<any>()));
   context.false(isMaybe({}));
   context.false(isMaybe(NaN));
   context.false(isMaybe(undefined));
@@ -13,15 +13,15 @@ test('isMaybe check if value is a Maybe', (context) => {
 // ..:: Maybe ::..
 
 test('Maybe typed value can be `null`, `undefined` or `T`', (context) => {
-  const valueA = Maybe<number>(undefined);
+  const valueA = createMaybe<number>(undefined);
 
   context.is(valueA.get(0), 0);
 
-  const valueB = Maybe<number>(null);
+  const valueB = createMaybe<number>(null);
 
   context.is(valueB.get(0), 0);
 
-  const valueC = Maybe<number>(10);
+  const valueC = createMaybe<number>(10);
 
   context.is(valueC.get(0), 10);
 });
@@ -33,7 +33,7 @@ test('Maybe.get returns value or placeholder if it\'s none', (context) => {
 
   context.is(size.get(0), 0);
 
-  const font = Maybe<string>('Operator Mono');
+  const font = Some<string>('Operator Mono');
 
   context.is(font.get(''), 'Operator Mono');
 });
@@ -41,7 +41,7 @@ test('Maybe.get returns value or placeholder if it\'s none', (context) => {
 // ..:: Maybe.map ::..
 
 test('Maybe.map call fn if value is some', (context) => {
-  const size = Maybe<number>(undefined);
+  const size = None<number>();
 
   let fnWasCalledForSize = false;
 
@@ -49,7 +49,7 @@ test('Maybe.map call fn if value is some', (context) => {
 
   context.false(fnWasCalledForSize);
 
-  const font = Maybe<string>('Operator Mono');
+  const font = Some<string>('Operator Mono');
 
   let fnWasCalledForFont = false;
 
@@ -59,7 +59,7 @@ test('Maybe.map call fn if value is some', (context) => {
 });
 
 test('Maybe.map returns empty Maybe if value is None', (context) => {
-  const value = Maybe<number>().map(() => 1000);
+  const value = None<number>().map(() => 1000);
 
   context.true(isMaybe(value));
 
@@ -71,7 +71,7 @@ test('Maybe.map returns empty Maybe if value is None', (context) => {
 });
 
 test('Maybe.map resolve Maybe<Maybe<any>> to Maybe<any>', (context) => {
-  const value = Maybe<number>(100).map((value) => Maybe(value));
+  const value = Some<number>(100).map((value) => Some(value));
 
   context.true(isMaybe(value));
 
@@ -120,9 +120,9 @@ test('Maybe.unwrap returns Maybe wrapped value', (context) => {
   const valueB = undefined;
   const valueC = 'It\'s not Nothing.';
 
-  context.is(Maybe(valueA).unwrap(), valueA);
-  context.is(Maybe(valueB).unwrap(), valueB);
-  context.is(Maybe(valueC).unwrap(), valueC);
+  context.is(createMaybe(valueA).unwrap(), valueA);
+  context.is(createMaybe(valueB).unwrap(), valueB);
+  context.is(createMaybe(valueC).unwrap(), valueC);
 });
 
 // ..:: Some ::..
