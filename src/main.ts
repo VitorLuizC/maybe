dd82fa27d1dae001c8abdbf1ec135d2dd84e9946
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2020-07-16 11:34:54 -0300

    🐛 fix tryCatch not export

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,4 @@ export { default as Some } from './Some.js';
 export { default as fromFalsy } from './fromFalsy.js';
 export { default as fromNullish } from './fromNullish.js';
 export { default as fromPredicate } from './fromPredicate.js';
+export { default as tryCatch } from './tryCatch.js';
